b934f3e75ade33d548ce63bcc24d9aef336b3e08
1n3d1t <119239780+1n3d1t@users.noreply.github.com>
2025-08-24 15:25:10 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
-   // URL del streaming de Listen2MyRadio
-  stream_url: 'http://healingsounds.radiostream321.com/;',
+  // URL del streaming de Listen2MyRadio
+  stream_url: 'http://176.227.215.27:23729/;',
   // URL base para imágenes y metadata en Synology (ajusta con tus datos reales)
   synology_base_url: 'http://tu-synology-ip:puerto/public/', // Ej: 'http://192.168.1.100:5000/public/'
   // Logo principal
